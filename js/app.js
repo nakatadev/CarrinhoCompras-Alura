8446bdbe473521de374892440a66e11fc3fde8e7
Rodrigo <nakataecomm@gmail.com>
2024-07-11 20:16:23 -0300

    Implementando JavaScript

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+let totalGeral = 0;
+
+limpar();
+
+function adicionar() {
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1]
+    let quantidade = document.getElementById('quantidade').value;
+
+    let subtotal = quantidade * valorUnitario; 
+
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos" id="lista-produtos">
+        <section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> Celular <span class="texto-azul">R$${valorUnitario}</span>
+        </section>
+      </section>`
+    
+    totalGeral = totalGeral + subtotal;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$${totalGeral}`;
+}
+
+function limpar() {
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
+
+
+}
